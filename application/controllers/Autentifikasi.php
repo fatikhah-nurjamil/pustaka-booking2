cc306cb48817d861bbcaf425308f4570d68d8853
fatikhah-nurjamil <fatikhahnurjamil21@gmail.com>
2021-11-29 13:18:18 +0700

    PUSH PERTEMUAN 11

--- application/controllers/Autentifikasi.php
+++ application/controllers/Autentifikasi.php
@@ -146,7 +146,7 @@ class Autentifikasi extends CI_Controller
                 'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
-                'is_active' => 0,
+                'is_active' => 1,
                 'tanggal_input' => time()
             ];
             $this->ModelUser->simpanData($data); //menggunakan model
@@ -160,6 +160,7 @@ class Autentifikasi extends CI_Controller
     {
         $this->session->unset_userdata('email');
         $this->session->unset_userdata('role_id');
+        
         $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" 
         role="alert">Anda telah logout!!</div>');
         redirect('Autentifikasi');
